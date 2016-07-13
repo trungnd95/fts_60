19c368fd5489ed89f03a66fae1bd70cee69fbb3d
lethuyb <lethuy.mta@gmail.com>
2016-07-13 08:02:38 +0700

    chang information

--- app/assets/javascripts/common.js.coffee
+++ app/assets/javascripts/common.js.coffee
@@ -3,3 +3,7 @@ $(document).ready ->
   $('#table-subject').dataTable
     retrieve: true,
     paging: false
+  $('#user_avatar').bind 'change', ->
+    size_in_megabytes = this.files[0].size/1024/1024
+    if size_in_megabytes > 5
+      alert('Maximum file size is 5MB. Please choose a smaller file.')
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+  load_and_authorize_resource
+
+  def update
+    if @user.update_attributes user_params
+      flash[:update_success] = t "views.devise.users.update"
+      redirect_to user_path current_user
+    else
+      render :edit
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit :name, :avatar, :email, :password,
+      :password_confirmation
+  end
+end
