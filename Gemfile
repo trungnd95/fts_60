135713d45d7b8d49f83c32f10d33a3a16b087389
lethuyb <lethuy.mta@gmail.com>
2016-07-13 08:02:38 +0700

    sign up/sign in/ logout

--- Gemfile
+++ Gemfile
@@ -34,6 +34,8 @@ gem "jquery-ui-rails"
 gem "turbolinks"
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem "jbuilder", "~> 2.0"
+gem "devise"
+gem "cancancan", "~> 1.10"
 # bundle exec rake doc:rails generates the API under doc/api.
 gem "sdoc", "~> 0.4.0", group: :doc
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ActiveRecord::Base
+  devise :database_authenticatable, :registerable,
+    :recoverable, :rememberable, :trackable, :validatable
+
+  enum role: [:admin, :user]
+
   has_many :examinations, dependent: :destroy
   has_many :activities, dependent: :destroy
-  has_many :questions, depandent: :destroy
+  has_many :questions, dependent: :destroy
 end
