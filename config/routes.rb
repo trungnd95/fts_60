62c9bd6cb8a3475923580304dff247f08d1b65cf
lethuyb <lethuy.mta@gmail.com>
2016-07-18 15:18:29 +0700

    admin can delete user

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,11 @@ Rails.application.routes.draw do
 
   root "static_pages#home"
   namespace :admin do
-    resources :users, only: :index
+    resources :users, only: [:index, :destroy]
     resources :subjects
     resources :questions
   end
   devise_for :users, controllers: {registrations: "users/registrations",
     omniauth_callbacks: "users/omniauth_callbacks"}
   resources :users, only: [:show, :edit, :update]
-
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160718102721) do
+ActiveRecord::Schema.define(version: 20160718073415) do
 
   create_table "activities", force: :cascade do |t|
     t.integer  "target_id"
@@ -100,7 +100,6 @@ ActiveRecord::Schema.define(version: 20160718102721) do
     t.string   "provider"
     t.string   "uid"
     t.time     "deleted_at"
-    t.time     "deteled_at"
   end
 
   add_index "users", ["deleted_at"], name: "index_users_on_deleted_at"
