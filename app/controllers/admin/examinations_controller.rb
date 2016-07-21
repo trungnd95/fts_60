75d9e270d4fec112cf81c9ae83c13b416b6f5014
Trung Nguyen <ndt8895@gmail.com>
2016-07-21 10:27:03 +0700

    FTS Admin check exam and send email by sidekiq

--- app/controllers/admin/examinations_controller.rb
+++ app/controllers/admin/examinations_controller.rb
@@ -1,11 +1,38 @@
 class Admin::ExaminationsController < ApplicationController
+  layout "admin"
   load_and_authorize_resource
 
   def index
-    @exams = Examination.includes([:subject, :user]).page(params[:page])
+    @examinations = Examination.includes([:subject, :user]).page(params[:page])
       .per Settings.per_page
   end
 
   def update
+    respond_to do |format|
+      if @examination.update_attributes examination_params
+        EmailWorker.perform_async examination_params[:id]
+        format.html do
+          flash[:success] = t "page.admin.exams.update.success"
+          redirect_to @exam
+        end
+        @score = "#{examination.results.is_corrects.count} /
+          #{examination.subject.question_number}"
+        format.json do
+          render json: {id: examination_params[:id], status: @examination.status,
+            score: @score}
+        end
+      else
+        format.html do
+          flash[:fail] = t "page.admin.exams.update.success"
+          redirect_to :back
+        end
+        format.json{render json: @examination.errors, status: :unprocessable_entity}
+      end
+    end
+  end
+
+  private
+  def examination_params
+    params.require(:examination).permit :status, :user_id, :subject_id
   end
 end
